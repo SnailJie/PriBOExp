fed73a71790b3a571222fd519dc0d6f1c8ce6123
root <snailren@126.com>
2018-02-05 10:48:20 +0800

    fix bug

--- modify.sh
+++ modify.sh
@@ -5,19 +5,20 @@
 
 if (($# < 5));then
         echo -e "Usge: Please input --cpu --ram --diskType --netType --count.\n --diskType 1:limit 0:unlimit  \n --netType 1:limit 0: unlimit"
-         exit 1
+        exit 1
 fi
 
 cpu=$1
-ram=$2
+((ram=$2*1024))
 diskType=$3
 netType=$4
 count=$5
 echo -e "Input configuration   \n cpu: $cpu,ram = $ram,diskType = $diskType,netType = $netType,count = $count "
 
+
 #==================modify hadoop============
 hadoop_path=$HADOOP_HOME
-hadoop_conf_path=$hadoop_path/etc/path
+hadoop_conf_path=$hadoop_path/etc/hadoop
 echo "Hadoop Path is $hadoop_path"
 
 #-----------replace ram----------
@@ -28,10 +29,9 @@ sed -i "s/cpuvalue/$cpu/g" $hadoop_conf_path/yarn-site.xml
  
 #===================== deploy yarn file==========
  
-cd $hadoop_path/etc/hadoop/
 for((i=1;i<=$count;i++));
 do 
-scp yarn-site.xml slave$i:`pwd`
+scp $hadoop_conf_path/yarn-site.xml slave$i:$hadoop_conf_path
 done
 
 bash $hadoop_path/sbin/stop-all.sh
@@ -40,12 +40,13 @@ sleep 10s
 cat /dev/null > $hadoop_conf_path/slaves  
 for((i=1;i<=$count;i++));
 do 
-echo slave$i > $hadoop_conf_path/slaves
+echo slave$i >> $hadoop_conf_path/slaves
 done
 
 #limit IO and network
 
 bash unlimit_iops.sh
+
 if(($diskType==1));then
 	bash limit_disk_iops.sh
 fi
